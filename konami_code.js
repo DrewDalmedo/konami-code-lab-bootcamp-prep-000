58a3d5cd1579583afb0ff7baf578c73b3b8e45bf
Andrew Dalmedo <andrewdalmedo@gmail.com>
2020-02-10 23:51:43 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -16,14 +16,13 @@ function onKeyDownHandler(e, index) {
   const key = e.key;
   
   if (key === codes[index]) {
+    index++;
     if (index === codes.length) {
       alert("KONAMI");
     }
-    else {
-      index = 0;
-    }
-    
-    index++;
+  }
+  else {
+    index = 0;
   }
 }
 
